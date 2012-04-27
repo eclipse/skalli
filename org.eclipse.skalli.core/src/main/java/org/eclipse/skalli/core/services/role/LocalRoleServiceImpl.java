729efb17059a9d24313caf77ab17a8e862f355fa
Michael Ochmann <michael.ochmann@sap.com>
2012-04-27 16:19:18 +0200

    Configurable Role Service
    * introduced LocalRoleServiceImpl which allows to manage groups via REST
      API path /api/config/roles
    * Configuration format:
      <roles>
      	<role>
      		<roleId>maintainer</roleId>
      		<users>
      		   <user>hugo</user>
      		   <user>foo</user>
      		   ...
      		</users>
      		<groups>
      		   <group>group4711</group>
      		   <group>the0815th</group>
      		   ...
      		</groups>
      	</role>
      	...
      </roles>
    
    Change-Id: I9ed56b2d5c3f6bfea6136ffc0fe2e8bbd2423638
    Signed-off-by: Michael Ochmann <michael.ochmann@sap.com>

---
+++ org.eclipse.skalli.core/src/main/java/org/eclipse/skalli/core/services/role/LocalRoleServiceImpl.java
@@ -0,0 +1,132 @@
+/*******************************************************************************
+ * Copyright (c) 2010, 2011 SAP AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     SAP AG - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.skalli.core.services.role;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.eclipse.skalli.services.configuration.ConfigurationService;
+import org.eclipse.skalli.services.event.EventCustomizingUpdate;
+import org.eclipse.skalli.services.event.EventListener;
+import org.eclipse.skalli.services.event.EventService;
+import org.eclipse.skalli.services.permit.PermitService;
+import org.eclipse.skalli.services.role.RoleService;
+import org.osgi.service.component.ComponentConstants;
+import org.osgi.service.component.ComponentContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LocalRoleServiceImpl implements RoleService, EventListener<EventCustomizingUpdate> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LocalRoleServiceImpl.class);
+
+    private ComponentContext context;
+    private ConfigurationService configService;
+
+    protected void activate(ComponentContext context) {
+        this.context = context;
+        invalidatePermits();
+        LOG.info(MessageFormat.format("[RoleService][type=local] {0} : activated",
+                (String) context.getProperties().get(ComponentConstants.COMPONENT_NAME)));
+    }
+
+    protected void deactivate(ComponentContext context) {
+        invalidatePermits();
+        this.context = null;
+        LOG.info(MessageFormat.format("[RoleService][type=local] {0} : deactivated",
+                (String) context.getProperties().get(ComponentConstants.COMPONENT_NAME)));
+    }
+
+    protected void bindConfigurationService(ConfigurationService configService) {
+        LOG.info(MessageFormat.format("bindConfigurationService({0})", configService)); //$NON-NLS-1$
+        this.configService = configService;
+        invalidatePermits();
+    }
+
+    protected void unbindConfigurationService(ConfigurationService configService) {
+        LOG.info(MessageFormat.format("unbindConfigurationService({0})", configService)); //$NON-NLS-1$
+        this.configService = null;
+        invalidatePermits();
+    }
+
+    protected void bindEventService(EventService eventService) {
+        LOG.info(MessageFormat.format("bindEventService({0})", eventService)); //$NON-NLS-1$
+        eventService.registerListener(EventCustomizingUpdate.class, this);
+    }
+
+    protected void unbindEventService(EventService eventService) {
+        LOG.info(MessageFormat.format("unbindEventService({0})", eventService)); //$NON-NLS-1$
+    }
+
+    private PermitService getPermitService() {
+        if (context != null) {
+            return (PermitService)context.locateService("PermitService"); //$NON-NLS-1$
+        }
+        return null;
+    }
+
+    @Override
+    public List<String> getRoles(String userId) {
+        if (configService == null || StringUtils.isBlank(userId)) {
+            return Collections.emptyList();
+        }
+        RolesConfig rolesConfig = configService.readCustomization(RolesResource.MAPPINGS_KEY, RolesConfig.class);
+        if (rolesConfig == null) {
+            return Collections.emptyList();
+        }
+        List<String> roles = new ArrayList<String>();
+        for (RoleConfig roleConfig: rolesConfig.getRoles()) {
+            if (roleConfig.getUsers().contains(userId)) {
+                roles.add(roleConfig.getRoleId());
+            }
+        }
+        return roles;
+    }
+
+    @Override
+    public List<String> getRoles(String... groups) {
+        if (configService == null || groups == null || groups.length == 0) {
+            return Collections.emptyList();
+        }
+        RolesConfig rolesConfig = configService.readCustomization(RolesResource.MAPPINGS_KEY, RolesConfig.class);
+        if (rolesConfig == null) {
+            return Collections.emptyList();
+        }
+        List<String> roles = new ArrayList<String>();
+        for (String groupName: groups) {
+            collectRoles(groupName, rolesConfig.getRoles(), roles);
+        }
+        return roles;
+    }
+
+    private void collectRoles(String groupName, List<RoleConfig> roleConfigs, List<String> roles) {
+        for (RoleConfig roleConfig: roleConfigs) {
+            if (roleConfig.getGroups().contains(groupName)) {
+                roles.add(roleConfig.getRoleId());
+            }
+        }
+    }
+
+    @Override
+    public void onEvent(EventCustomizingUpdate event) {
+        invalidatePermits();
+    }
+
+    private void invalidatePermits() {
+        PermitService permitService = getPermitService();
+        if (permitService != null) {
+            permitService.logoutAll();
+        }
+    }
+}
---
+++ org.eclipse.skalli.core/src/main/java/org/eclipse/skalli/core/services/role/RoleConfig.java
@@ -0,0 +1,62 @@
+/*******************************************************************************
+ * Copyright (c) 2010, 2011 SAP AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     SAP AG - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.skalli.core.services.role;
+
+import java.util.Collection;
+import java.util.TreeSet;
+
+import org.eclipse.skalli.commons.CollectionUtils;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamImplicit;
+
+@XStreamAlias("role")
+public class RoleConfig {
+
+    private String roleId;
+
+    @XStreamImplicit(itemFieldName = "user")
+    private TreeSet<String> users = new TreeSet<String>();
+
+    @XStreamImplicit(itemFieldName = "group")
+    private TreeSet<String> groups = new TreeSet<String>();
+
+    public RoleConfig() {
+    }
+
+    public RoleConfig(String roleId, Collection<String> users, Collection<String> groups) {
+        this.roleId = roleId;
+        if (CollectionUtils.isNotBlank(users)) {
+            getUsers().addAll(users);
+        }
+        if (CollectionUtils.isNotBlank(groups)) {
+            getGroups().addAll(groups);
+        }
+    }
+
+    public String getRoleId() {
+        return roleId;
+    }
+
+    public synchronized TreeSet<String> getUsers() {
+        if (users == null) {
+            users = new TreeSet<String>();
+        }
+        return users;
+    }
+
+    public synchronized TreeSet<String> getGroups() {
+        if (groups == null) {
+            groups = new TreeSet<String>();
+        }
+        return groups;
+    }
+}
