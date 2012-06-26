1efbfdf2e6a1021df888dd5bc7e5327dd6b9c1a5
Michael Ochmann <michael.ochmann@sap.com>
2012-06-26 13:20:04 +0200

    Support for backup restore via /api/admin/backup
    * upload of backups with PUT requests, e.g. with curl:
    
        curl -u <user>:<password> --basic --upload-file
             "tmp/backup.zip" "http://localhost:8080/api/admin/backup"
    
    * introduced query params include=<comma-separated-list> and
      exclude=<comma-separated-list> for both GET/PUT to filter categories:
      - if include list is given, only those categories are saved/restored
      - if exclude list is given, those categories are not saved/restored
      same category in both lists: exclude wins
    * PUT/restore rejects attempts to overwrite non-empty storage
      categories by default (status code 412); action=overwrite param must
      be added to enforce restore, or the non-empty categories must be
      excluded explicitly
    * improved the error handling, introduced logging
    
    Change-Id: I5ac3771c21bc45e91b4647a4c95c7f592bfc6f12
    Signed-off-by: Michael Ochmann <michael.ochmann@sap.com>

--- org.eclipse.skalli.core/src/main/java/org/eclipse/skalli/api/rest/internal/admin/ProjectBackupResource.java
+++ org.eclipse.skalli.core/src/main/java/org/eclipse/skalli/api/rest/internal/admin/ProjectBackupResource.java
@@ -15,17 +15,23 @@ import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.text.MessageFormat;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+import org.eclipse.skalli.commons.CollectionUtils;
 import org.eclipse.skalli.core.internal.persistence.xstream.FileStorageService;
 import org.eclipse.skalli.services.ServiceFilter;
 import org.eclipse.skalli.services.Services;
 import org.eclipse.skalli.services.configuration.ConfigurationProperties;
 import org.eclipse.skalli.services.extension.rest.ResourceBase;
+import org.eclipse.skalli.services.extension.rest.RestUtils;
+import org.eclipse.skalli.services.persistence.PersistenceService;
 import org.eclipse.skalli.services.persistence.StorageException;
 import org.eclipse.skalli.services.persistence.StorageService;
 import org.restlet.data.Disposition;
@@ -34,22 +40,145 @@ import org.restlet.data.Status;
 import org.restlet.representation.OutputRepresentation;
 import org.restlet.representation.Representation;
 import org.restlet.resource.Get;
+import org.restlet.resource.Put;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ProjectBackupResource extends ResourceBase {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ProjectBackupResource.class);
+
     private static final String FILE_NAME = "backup.zip"; //$NON-NLS-1$
 
+    private static final String EXCLUDE_PARAM = "exclude"; //$NON-NLS-1$
+    private static final String INCLUDE_PARAM = "include"; //$NON-NLS-1$
+
+    private static final String ACTION_PARAM = "action"; //$NON-NLS-1$
+    private static final String ACTION_OVERWRITE = "overwrite"; //$NON-NLS-1$
+
+    @SuppressWarnings("nls")
+    private static final Set<String> CATEGORIES = CollectionUtils.asSet("customization", "Project", "Issues",
+            "Favorites", "User", "Group");
+
     @Get
-    public Representation retrieve() {
+    public Representation backup() {
         StorageService storageService = getStorageService();
         if (storageService == null) {
             return createStatusMessage(Status.SERVER_ERROR_INTERNAL, "No storage service available");
         }
-        ZipRepresentation zipRepresentation = new ZipRepresentation(storageService);
+        Set<String> included = getCategories(INCLUDE_PARAM);
+        Set<String> excluded = getCategories(EXCLUDE_PARAM);
+        ZipOutputRepresentation zipRepresentation = new ZipOutputRepresentation(storageService, included, excluded);
         Disposition disposition = new Disposition(Disposition.TYPE_ATTACHMENT);
         disposition.setFilename(FILE_NAME);
         zipRepresentation.setDisposition(disposition);
         return zipRepresentation;
+    }
+
+    @Put
+    public Representation restore(Representation entity) {
+        StorageService storageService = getStorageService();
+        if (storageService == null) {
+            return createStatusMessage(Status.SERVER_ERROR_INTERNAL, "No storage service available");
+        }
+
+        String action = getQuery().getValues(ACTION_PARAM);
+        Set<String> included = getCategories(INCLUDE_PARAM);
+        Set<String> excluded = getCategories(EXCLUDE_PARAM);
+
+        Set<String> accepted = new HashSet<String>();
+        Set<String> rejected = new HashSet<String>();
+        for (String category: CATEGORIES) {
+            if (included.size() > 0 && !included.contains(category)) {
+                continue;
+            }
+            if (excluded.size() > 0 && excluded.contains(category)) {
+                continue;
+            }
+            try {
+                if (ACTION_OVERWRITE.equals(action) || storageService.keys(category).isEmpty()) {
+                    accepted.add(category);
+                } else {
+                    rejected.add(category);
+                }
+            } catch (StorageException e) {
+                LOG.error("Failed to retrieve keys for category " + category, e);
+                return createStatusMessage(Status.SERVER_ERROR_INTERNAL, "Failed to store the attachment");
+            }
+        }
+        if (rejected.size() > 0) {
+            return createStatusMessage(Status.CLIENT_ERROR_PRECONDITION_FAILED, MessageFormat.format(
+                    "WARNING: Restore might overwrite existing project data in the folling categories:\n{0}\n" +
+                    "Either exclude these categories from the restore with a \"exclude=<comma-separated-list>\" parameter"
+                    + " or enforce the restore with \"action=overwrite\".",
+                    CollectionUtils.toString(rejected, '\n')));
+        }
+        if (accepted.isEmpty()) {
+            return createStatusMessage(Status.SUCCESS_OK, "Nothing to do");
+        }
+
+        ZipInputStream zipStream = null;
+        try {
+            zipStream = new ZipInputStream(entity.getStream());
+            ZipEntry entry = zipStream.getNextEntry();
+            while (entry != null) {
+                try {
+                    if (!entry.isDirectory()) {
+                        String entryName = entry.getName().replace('\\', '/');
+                        String[] parts = StringUtils.split(entryName, '/');
+                        if (parts.length != 2) {
+                            LOG.info(MessageFormat.format("Restore: {0} is not recognized as entity key", entryName));
+                            continue;
+                        }
+                        // ensure that the category of the entry, i.e. the directory name,
+                        // is in the set of accepted categories
+                        String category = parts[0];
+                        String key = parts[1];
+                        if (accepted.contains(category)) {
+                            if (key.endsWith(".xml")) { //$NON-NLS-1$
+                                key = key.substring(0, key.length() - 4);
+                            }
+                            try {
+                                storageService.write(category, key, zipStream);
+                            } catch (StorageException e) {
+                                LOG.error(MessageFormat.format(
+                                        "Failed to store entity with key {0} and category {1}",
+                                        key, category), e);
+                                return createStatusMessage(Status.SERVER_ERROR_INTERNAL, "Failed to store the attachment");
+                            }
+                        } else {
+                            LOG.info(MessageFormat.format("Restore: Excluded {0} (category ''{1}'' not accepted)", key, category));
+                        }
+                    }
+                } finally {
+                    zipStream.closeEntry();
+                    entry = zipStream.getNextEntry();
+                }
+            }
+        } catch (IOException e) {
+            LOG.error("I/O error when reading the attachment", e);
+            return createStatusMessage(Status.SERVER_ERROR_INTERNAL, "Failed to store the attachment");
+        } finally {
+            IOUtils.closeQuietly(zipStream);
+        }
+
+        // ensure that the persistence service attached to the storage
+        // refreshes all caches and reloads all entities
+        PersistenceService persistenceService = Services.getService(PersistenceService.class);
+        if (persistenceService != null) {
+            persistenceService.refreshAll();
+        }
+
+        return createStatusMessage(Status.SUCCESS_OK, "Attachment successfully stored");
+    }
+
+    private Set<String> getCategories(String paramId) {
+        String[] categories = null;
+        String ignoreAttribute = getQuery().getValues(paramId);
+        if (ignoreAttribute != null) {
+            categories = StringUtils.split(ignoreAttribute, RestUtils.PARAM_LIST_SEPARATOR);
+        }
+        return CollectionUtils.asSet(categories);
     }
 
     private StorageService getStorageService() {
@@ -64,15 +193,19 @@ public class ProjectBackupResource extends ResourceBase {
         return storageServices.size() > 0? storageServices.iterator().next() : null;
     }
 
-    private static class ZipRepresentation extends OutputRepresentation {
+    private static class ZipOutputRepresentation extends OutputRepresentation {
 
         private static final int BUFFER = 2048;
 
         private StorageService storageService;
+        private Set<String> included;
+        private Set<String> excluded;
 
-        public ZipRepresentation(StorageService storageService) {
+        public ZipOutputRepresentation(StorageService storageService, Set<String> included, Set<String> excluded) {
             super(MediaType.APPLICATION_ZIP);
             this.storageService = storageService;
+            this.included = included;
+            this.excluded = excluded;
         }
 
         @Override
@@ -80,12 +213,19 @@ public class ProjectBackupResource extends ResourceBase {
             ZipOutputStream out = null;
             try {
                 out = new ZipOutputStream(new BufferedOutputStream(paramOutputStream));
-                read("customization", out); //$NON-NLS-1$
-                read("Project", out); //$NON-NLS-1$
-                read("Issues", out); //$NON-NLS-1$
-                read("Favorites", out); //$NON-NLS-1$
-                read("User", out); //$NON-NLS-1$
-                read("Group", out); //$NON-NLS-1$
+                for (String category: CATEGORIES) {
+                    // if there is an explicit include list, ensure that the
+                    // category is contained in that list
+                    if (included.size() > 0 && !included.contains(category)) {
+                        continue;
+                    }
+                    // if there is an explicit exclude list, ensure that the
+                    // category is not contained in that list
+                    if (excluded.size() > 0 && excluded.contains(category)) {
+                        continue;
+                    }
+                    write(category, out);
+                }
             } catch (StorageException e) {
                 throw new IOException(e);
             } finally {
@@ -93,7 +233,7 @@ public class ProjectBackupResource extends ResourceBase {
             }
         }
 
-        private void read(String category, ZipOutputStream out) throws StorageException, IOException {
+        private void write(String category, ZipOutputStream out) throws StorageException, IOException {
             List<String> keys = storageService.keys(category);
             BufferedInputStream origin = null;
             for (String key: keys) {
@@ -112,4 +252,5 @@ public class ProjectBackupResource extends ResourceBase {
             }
         }
     }
+
 }
