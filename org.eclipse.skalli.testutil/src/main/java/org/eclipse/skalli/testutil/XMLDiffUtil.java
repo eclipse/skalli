fa81b4722b6d95cea790970c37eceff4a69f973a
Michael Ochmann <michael.ochmann@sap.com>
2014-12-05 11:15:16 +0100

    Order of projects in REST response should not matter for tests
    The REST API does not guarantee a certain order of projects or
    extensions of a project, so the XML tests should not expect
    a certain order.
    
    Change-Id: Id2838ba75afcc9e0b39fdb5255c979c3b8a7544b
    Signed-off-by: Michael Ochmann <michael.ochmann@sap.com>

--- org.eclipse.skalli.testutil/src/main/java/org/eclipse/skalli/testutil/XMLDiffUtil.java
+++ org.eclipse.skalli.testutil/src/main/java/org/eclipse/skalli/testutil/XMLDiffUtil.java
@@ -10,67 +10,154 @@
  *******************************************************************************/
 package org.eclipse.skalli.testutil;
 
-import java.io.StringWriter;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Result;
-import javax.xml.transform.Source;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
+import java.util.Arrays;
 
+import javax.xml.transform.TransformerException;
+
+import org.custommonkey.xmlunit.ComparisonController;
 import org.custommonkey.xmlunit.Diff;
+import org.custommonkey.xmlunit.Difference;
+import org.custommonkey.xmlunit.DifferenceConstants;
+import org.custommonkey.xmlunit.DifferenceEngine;
+import org.custommonkey.xmlunit.DifferenceListener;
 import org.custommonkey.xmlunit.XMLUnit;
-import org.junit.Assert;
+import org.eclipse.skalli.commons.XMLUtils;
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 
 @SuppressWarnings("nls")
 public class XMLDiffUtil {
 
-    /**
-     * Asserts that the given {@link Document documents} are equal.
-     */
-    public static void assertEquals(Document expected, Document actual, boolean ignoreWhitespace) throws Exception {
-        XMLUnit.setIgnoreWhitespace(ignoreWhitespace);
-        Diff diff = XMLUnit.compareXML(expected, actual);
-        Assert.assertTrue(detailsToString(diff, expected, actual), diff.similar());
-        Assert.assertTrue(detailsToString(diff, expected, actual), diff.identical());
+    public static class DiffOptions implements ComparisonController, DifferenceListener {
+
+        private int[] list = new int[8];
+        private int size = 0;
+
+        private boolean identical = true;
+
+        public DiffOptions(int...options) {
+            append(options);
+        }
+
+        public DiffOptions(DiffOptions base, int...options) {
+            append(base.list);
+            append(options);
+        }
+
+        public void append(int... options) {
+            if (options != null && options.length > 0) {
+                if (size + options.length > list.length) {
+                    int[] grown = new int[list.length + options.length];
+                    System.arraycopy(list, 0, grown, 0, list.length);
+                    list = grown;
+                }
+                System.arraycopy(list, size, options, 0, options.length);
+                size += options.length;
+                Arrays.sort(list);
+            }
+        }
+
+        @Override
+        public int differenceFound(Difference difference) {
+            int result = RETURN_IGNORE_DIFFERENCE_NODES_IDENTICAL;
+            if (Arrays.binarySearch(list, difference.getId()) < 0) {
+                identical = false;
+                result = RETURN_ACCEPT_DIFFERENCE;
+            }
+            return result;
+        }
+
+        @Override
+        public void skippedComparison(Node control, Node test) {
+        }
+
+        @Override
+        public boolean haltComparison(Difference afterDifference) {
+            return !identical;
+        }
+
+        public boolean identical() {
+            return identical;
+        }
     }
 
-    protected static String detailsToString(Diff diff, Document expected, Document actual) throws Exception {
+    /**
+     * Ignore comments, processing instructions and CDATA sections.
+     */
+    public static final DiffOptions DEFAULT_DIFF_OPTIONS = new DiffOptions(
+            DifferenceConstants.COMMENT_VALUE_ID,
+            DifferenceConstants.PROCESSING_INSTRUCTION_TARGET_ID,
+            DifferenceConstants.PROCESSING_INSTRUCTION_DATA_ID,
+            DifferenceConstants.CDATA_VALUE_ID);
+
+    /**
+     * Asserts that the given {@link Document documents} are {@link Diff#similar() similiar}
+     * and {@link Diff#identical() identical}.
+     *
+     * @param expected  the expected document.
+     * @param actual  the actual document produced by the test.
+     */
+    public static void assertEquals(Document expected, Document actual, boolean b) {
+        XMLUnit.setIgnoreWhitespace(true);
+        DifferenceEngine engine = new DifferenceEngine(new DiffOptions(DEFAULT_DIFF_OPTIONS,
+                DifferenceConstants.CHILD_NODELIST_SEQUENCE_ID));
+        Diff diff = new Diff(expected, actual, engine);
+        assertSimilar(expected, actual, diff);
+        assertIdentical(expected, actual, diff);
+    }
+
+    /**
+     * Asserts that the given {@link Document documents} are {@link Diff#similar() similiar}
+     * according to the given {@link Diff}.
+     *
+     * @param expected  the expected document.
+     * @param actual  the actual document produced by the test.
+     * @param diff  the {@link Diff} of the documents.
+     */
+    public static void assertSimilar(Document expected, Document actual, Diff diff) {
+        assertTrue(detailsToString(expected, actual, diff), diff.similar());
+    }
+
+    /**
+     * Asserts that the given {@link Document documents} are {@link Diff#identical() identical}
+     * according to the given {@link Diff}.
+     *
+     * @param expected  the expected document.
+     * @param actual  the actual document produced by the test.
+     * @param diff  the {@link Diff} of the documents.
+     */
+    public static void assertIdentical(Document expected, Document actual, Diff diff) {
+        assertTrue(detailsToString(expected, actual, diff), diff.identical());
+    }
+
+    /**
+     * Renders a diff report for the given  {@link Document documents}.
+     *
+     * @param expected  the expected document.
+     * @param actual  the actual document produced by the test.
+     * @param diff  the {@link Diff} of the documents.
+     * @return  the difference of the documents, as well as the string representations
+     * of both documents.
+     */
+    public static String detailsToString(Document expected, Document actual, Diff diff) {
         StringBuffer sb = new StringBuffer();
         diff.appendMessage(sb);
 
-        if (expected != null) {
-            sb.append("\nExpected:\n");
-            sb.append(toString(expected)).append("\n");
+        try {
+            if (expected != null) {
+                sb.append("\nExpected:\n");
+                sb.append(XMLUtils.documentToString(expected)).append("\n");
+            }
+            if (actual != null) {
+                sb.append("\nActual:\n");
+                sb.append(XMLUtils.documentToString(actual)).append("\n");
+            }
+        } catch (TransformerException e) {
+            fail("Failed to render an XML document:" + e.getMessageAndLocation());
         }
-        if (actual != null) {
-            sb.append("\nActual:\n");
-            sb.append(toString(actual)).append("\n");
-        }
-
         return sb.toString();
-    }
-
-    protected static String toString(Document doc) throws Exception {
-        TransformerFactory factory = TransformerFactory.newInstance();
-        Transformer transformer = factory.newTransformer();
-        StringWriter writer = new StringWriter();
-        Result result = new StreamResult(writer);
-        Source source = new DOMSource(doc);
-        transformer.transform(source, result);
-        writer.close();
-
-        return writer.toString();
-    }
-
-    protected static DocumentBuilder getDocumentBuilder() throws ParserConfigurationException {
-        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-        return docBuilder;
     }
 }
