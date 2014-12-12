5ceeeee2accfcf1ba1bd36b1860eb9ce0d390abf
Michael Ochmann <michael.ochmann@sap.com>
2014-12-12 11:07:14 +0100

    [findbugs] StorageCommandTest should not rely on platform encoding
    Change-Id: Ibde4b7f21bf31e1a7248c4bb45c54cf9076c1db3
    Signed-off-by: Michael Ochmann <michael.ochmann@sap.com>

--- org.eclipse.skalli.commands.test/src/main/java/org/eclipse/skalli/commands/StorageCommandTest.java
+++ org.eclipse.skalli.commands.test/src/main/java/org/eclipse/skalli/commands/StorageCommandTest.java
@@ -11,12 +11,15 @@
 package org.eclipse.skalli.commands;
 
 import static org.easymock.EasyMock.createMock;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.ByteArrayInputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
 
 import org.eclipse.osgi.framework.console.CommandInterpreter;
 import org.eclipse.skalli.testutil.HashMapStorageService;
+import org.eclipse.skalli.testutil.HashMapStorageService.Key;
 import org.eclipse.skalli.testutil.TestUUIDs;
 import org.junit.Test;
 
@@ -25,30 +28,33 @@ public class StorageCommandTest {
     static final String CATEGORY_PROJECT = "Project";
     static final String KEY_1 = TestUUIDs.TEST_UUIDS[0].toString();
     static final String KEY_2 = TestUUIDs.TEST_UUIDS[1].toString();
-    static final String TEST_CONTENT_1 = "test content bla bal";
-    static final String TEST_CONTENT_2 = "test content hello world";
+    static final byte[] TEST_CONTENT_1 = getBytesUTF8("test content bla bal");
+    static final byte[] TEST_CONTENT_2 = getBytesUTF8("test content hello world");
 
     @Test
     public void testCopy() throws Exception {
         HashMapStorageService source = new HashMapStorageService();
-        source.write(CATEGORY_PROJECT, KEY_1, new ByteArrayInputStream(
-                TEST_CONTENT_1.getBytes("UTF-8")));
-
-        source.write(CATEGORY_PROJECT, KEY_2, new ByteArrayInputStream(
-                TEST_CONTENT_2.getBytes("UTF-8")));
+        source.write(CATEGORY_PROJECT, KEY_1, new ByteArrayInputStream(TEST_CONTENT_1));
+        source.write(CATEGORY_PROJECT, KEY_2, new ByteArrayInputStream(TEST_CONTENT_2));
 
         HashMapStorageService destination = new HashMapStorageService();
         CommandInterpreter intr = createMock(CommandInterpreter.class);
         StorageCommand.copy(source, destination, CATEGORY_PROJECT, intr);
 
-        assertEquals(
+        assertTrue(Arrays.equals(
                 TEST_CONTENT_1,
-                new String(destination.asMap().get(
-                        new HashMapStorageService.Key(CATEGORY_PROJECT, KEY_1))));
-        assertEquals(
+                destination.asMap().get(new Key(CATEGORY_PROJECT, KEY_1))));
+        assertTrue(Arrays.equals(
                 TEST_CONTENT_2,
-                new String(destination.asMap().get(
-                        new HashMapStorageService.Key(CATEGORY_PROJECT, KEY_2))));
+                destination.asMap().get(new Key(CATEGORY_PROJECT, KEY_2))));
+    }
+
+    private static byte[] getBytesUTF8(String s) {
+        try {
+            return s.getBytes("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new IllegalStateException("UTF-8");
+        }
     }
 
 }
