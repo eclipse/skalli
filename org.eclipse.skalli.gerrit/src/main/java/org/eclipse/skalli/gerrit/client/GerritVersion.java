abd545c23f2d76cae8ca3e80027a61c420dc27ac
Michael Ochmann <michael.ochmann@sap.com>
2015-04-28 13:13:26 +0200

    Simplify GerritVersion implementation
    Adding another if for each new supported version was quite ugly,
    so this patch revises the implementation.
    
    Change-Id: I6cdc4cfb9a79d1aedc13c35e5a25517b4b12d7d1
    Signed-off-by: Michael Ochmann <michael.ochmann@sap.com>

--- org.eclipse.skalli.gerrit/src/main/java/org/eclipse/skalli/gerrit/client/GerritVersion.java
+++ org.eclipse.skalli.gerrit/src/main/java/org/eclipse/skalli/gerrit/client/GerritVersion.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010-2014 SAP AG and others.
+ * Copyright (c) 2010-2015 SAP AG and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,52 +12,30 @@ package org.eclipse.skalli.gerrit.client;
 
 import org.apache.commons.lang.StringUtils;
 
-
+@SuppressWarnings("nls")
 public enum GerritVersion {
-    GERRIT_UNKNOWN_VERSION,
-    GERRIT_2_0_X, GERRIT_2_1_X, GERRIT_2_1_7, GERRIT_2_1_8, GERRIT_2_2_X,
-    GERRIT_2_2_2, GERRIT_2_3_X, GERRIT_2_4_X, GERRIT_2_5_X, GERRIT_2_6_X,
-    GERRIT_2_7_X;
+    GERRIT_UNKNOWN_VERSION(""),
+    GERRIT_2_0_X("2.0"), GERRIT_2_1_7("2.1.7"), GERRIT_2_1_8("2.1.8"), GERRIT_2_1_X("2.1"), GERRIT_2_2_2("2.2.2"),
+    GERRIT_2_2_X("2.2"), GERRIT_2_3_X("2.3"), GERRIT_2_4_X("2.4"), GERRIT_2_5_X("2.5"), GERRIT_2_6_X("2.6"),
+    GERRIT_2_7_X("2.7");
 
-    @SuppressWarnings("nls")
+    private String versionString;
+
+    private GerritVersion(String versionString) {
+        this.versionString = versionString;
+    }
+
     public static GerritVersion asGerritVersion(String s) {
         if (StringUtils.isBlank(s)) {
             return GERRIT_UNKNOWN_VERSION;
         }
-        if (s.equals("2.1.7") || s.startsWith("2.1.7.") || s.startsWith("2.1.7-")) {
-            return GERRIT_2_1_7;
+        for (GerritVersion next: values()) {
+            if (s.equals(next.versionString) || s.startsWith(next.versionString + ".")
+                    || s.startsWith(next.versionString + "-")) {
+                return next;
+            }
         }
-        if (s.equals("2.1.8") || s.startsWith("2.1.8.") || s.startsWith("2.1.8-")) {
-            return GERRIT_2_1_8;
-        }
-        if (s.equals("2.2.2") || s.startsWith("2.2.2.") || s.startsWith("2.2.2-")) {
-            return GERRIT_2_2_2;
-        }
-        if (s.equals("2.0") || s.startsWith("2.0.") || s.startsWith("2.0-")) {
-            return GERRIT_2_0_X;
-        }
-        if (s.equals("2.1") || s.startsWith("2.1.") || s.startsWith("2.1-")) {
-            return GERRIT_2_1_X;
-        }
-        if (s.equals("2.2") || s.startsWith("2.2.") || s.startsWith("2.2-")) {
-            return GERRIT_2_2_X;
-        }
-        if (s.equals("2.3") || s.startsWith("2.3.") || s.startsWith("2.3-")) {
-            return GERRIT_2_3_X;
-        }
-        if (s.equals("2.4") || s.startsWith("2.4.") || s.startsWith("2.4-")) {
-            return GERRIT_2_4_X;
-        }
-        if (s.equals("2.5") || s.startsWith("2.5.") || s.startsWith("2.5-")) {
-            return GERRIT_2_5_X;
-        }
-        if (s.equals("2.6") || s.startsWith("2.6.") || s.startsWith("2.6-")) {
-            return GERRIT_2_6_X;
-        }
-        if (s.equals("2.7") || s.startsWith("2.7.") || s.startsWith("2.7-")) {
-            return GERRIT_2_7_X;
-        }
-        // fall back to a resonable default version
+        // fall back to a reasonable default version
         return GERRIT_2_4_X;
     }
 
